4abe47aea8acaf7a87a6506cde89d412b64bcbc7
Cheick Abdoul-Razack NACRO <nacro.cheick@outlook.fr>
2020-04-08 01:00:22 +0000

    add post and getbyid function

--- server.js
+++ server.js
@@ -16,13 +16,45 @@ app.listen(port, ()=>{
 
 // CREATE
 app.post('/users',(req,res)=>{
-  // User.create()
-})
+  let d = req.body; 
+  User.create(
+    {
+      name:d.name,
+      email:d.email,
+      password:d.password
+    },
+    (err,data)=>{
+    if (err){
+      res.json({success: false,message: err})
+    } else if (!data){
+      res.json({success: false,message: "Not Found"})
+    } else {
+      res.json({success: true,data: data})
+    }
+  });
+});
 
 app.route('/users/:id')
 // READ
 .get((req,res)=>{
-  // User.findById()
+  User.findById(req.params.id,(err,data)=>{
+    if (err){
+      res.json({
+        success: false,
+        message: err
+      })
+    } else if (!data){
+      res.json({
+        success: false,
+        message: "Not Found"
+      })
+    } else {
+      res.json({
+        success: true,
+        data: data
+      })
+    }
+  })
 })
 // UPDATE
 .put((req,res)=>{
